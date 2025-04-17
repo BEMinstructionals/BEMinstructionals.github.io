1dd5d5d4cbd1739ad89e43f91a5f5dee340807c0
mitchaldichter <73209151+mitchaldichter@users.noreply.github.com>
2025-04-17 10:55:23 -0700

    renamed BEM File Editor Setup.exe to BEM-File-Editor-Setup.exe

--- windows/create_installer.bat
+++ windows/create_installer.bat
@@ -9,7 +9,7 @@ python -m venv env
 env\Scripts\pip3.exe install pyside6 pyinstaller
 env\Scripts\pyinstaller.exe windows.spec
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" create_windows_installer.iss
-move "Output\BEM File Editor Setup.exe" "BEM File Editor Setup.exe"
+move "Output\BEM File Editor Setup.exe" "BEM-File-Editor-Setup.exe"
 rmdir /s /q build
 rmdir /s /q dist
 rmdir /s /q env
